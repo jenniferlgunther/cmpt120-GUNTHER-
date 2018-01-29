3cb31dad6c607eca19fabbffa4db051d4d719faa
Jennifer Gunther <jennifergunther@148-100-160-213.FoxNet.marist.edu>
2018-01-29 17:55:16 -0500

    Solution to Curiosity rover exercise

---
+++ Lab Assignments/rover.py
@@ -0,0 +1,15 @@
+# Introduction to Programming
+# Jennifer Gunther
+# 29 January 2018
+
+def rover ():
+    distance = eval(input ("Enter a distance in miles"))
+    time = distance / 186000
+    print ("The time in seconds is", time)
+    
+
+
+
+
+
+    
