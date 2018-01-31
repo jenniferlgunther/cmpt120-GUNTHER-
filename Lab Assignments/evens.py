2ec8a5cf2c77babca44c6001937a7ac5060c55c3
Jennifer Gunther <jennifergunther@148-100-160-213.FoxNet.marist.edu>
2018-01-31 13:40:23 -0500

    The actual Solution to evens loop exercise

--- Lab Assignments/evens.py
+++ Lab Assignments/evens.py
@@ -2,9 +2,25 @@
 # Jennifer Gunther
 # 29 January 2018
 
+print ("This program will print integers included in range")
 for i in range(10):
     print(i)
-for i in range(20):
+
+# Making empty lists to append even numbers
+even_numbers = []
+
+def evens():
+    print ("This program will print all even integers")
+    for i in range(20):
+        if i % 2 == 0:
+            even_numbers.append(i)
+    print ("Even numbers:", even_numbers)
+    
+    
+    
+
+
+    
     
     
 
