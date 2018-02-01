cb7f0811bfc5f01159235a169a96ea0afcecce4d
Jennifer Gunther <jennifergunther@148-100-160-213.FoxNet.marist.edu>
2018-01-31 19:50:03 -0500

    Solution to Madlib exercise

---
+++ Lab Assignments/madlib.py
@@ -0,0 +1,16 @@
+# Introduction to Programming
+# Jennifer Gunther
+# 31 January 2018
+
+print ("This program is going to generate a a funny madlib statement")
+
+def madlib():
+    print ("Welcome to my madlib statement")
+    name = input("Enter a name")
+    verb = input("Enter a verb")
+    adjective = input("Enter an adjective")
+    place = input("Enter a place")
+
+    print("Our good friend", name, "was going for a", verb, "when the", adjective, "flamingo jumped into the", place)
+          
+
