1d8a651e7804c597e09bb11e48ee3c7d453a59c3
jenniferlgunther <35709424+jenniferlgunther@users.noreply.github.com>
2018-02-23 17:21:18 -0500

    GUI

---
+++ GUNTHER_calculatorfunctions.py
@@ -0,0 +1,30 @@
+#Introduction to Programming
+#Jennifer Gunther
+#23 February 2018
+
+
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def change_sign(num1):
+    result = (num1 * -1)
+    return result
+
+def square(num1):
+    result = num1 * num1
+    return result
+
