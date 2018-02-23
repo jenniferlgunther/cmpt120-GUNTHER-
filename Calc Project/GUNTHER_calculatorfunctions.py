40179422e472f6c175cefc7bc22fca6b01b4e381
Jennifer Gunther <jennifergunther@148-100-160-213.FoxNet.marist.edu>
2018-02-23 11:25:45 -0500

    Calculator functions in folder labeled "Calc Project"

---
+++ Calc Project/GUNTHER_calculatorfunctions.py
@@ -0,0 +1,22 @@
+#Introduction to Programming
+#Jennifer Gunther
+#23 February 2018
+
+
+
+#This function adds two numbers 
+def add(x, y):
+   return result
+
+# This function subtracts two numbers 
+def subtract(x, y):
+   return result
+
+# This function multiplies two numbers
+def multiply(x, y):
+   return result
+
+# This function divides two numbers
+def divide(x, y):
+   return result
+
