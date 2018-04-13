a85452bfe716a3b7d6514aa1739902441538284f
jenniferlgunther <35709424+jenniferlgunther@users.noreply.github.com>
2018-04-13 14:36:47 -0400

    Implemented emotions

---
+++ Lab Assignments/personality.py
@@ -0,0 +1,33 @@
+#Jennifer Gunther
+#Introduction to Programming
+#Professor Arias
+#13 April 2018
+
+#feelings
+#0 = "anger"
+#1 = "disgust"
+#2 = "happiness"
+#3 = "sadness"
+#4 = "surprise"
+#5 = "fear"
+
+#0 = "reward"
+#1 = "punish"
+#2 = "threaten"
+#3 = "joke"
+
+import random
+
+emotionMatrix = [
+    [4, 0, 0. 2]
+    [2, 0, 0, 2]
+    [2, 3, 5, 2]
+    [2, 3, 3, 2]
+    [2, 3, 5, 2]
+    [4, 3, 5, 2]
+]
+
+feelingslist = ["anger", "disgust", "happiness", "sadness", "surprise", "fear"]
+actionslist = ["reward", "punish", "threaten", "joke"]
+reactionlist = ["Grrr! That made me angry!", "Yuck! that is disgusting", "Wooohooo! that made me happy", "Boohoo, you are going to make me cry", "Wow! Very Surprising",  "Now I'm scared"]
+
